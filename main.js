35db1b9e6486ab255ee921c815c72b1cccf990d5
Yutong Pan <yutonp@uw.edu>
2015-12-06 12:49:08 -0800

    cirlce with sound

---
+++ main.js
@@ -0,0 +1,20 @@
+// microphone input
+var mic;
+
+// Set up cannot be named differently, constructs canvas
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  background(220);
+  // microphone volume input
+  mic = new p5.AudioIn();
+  mic.start();
+
+}
+
+// draw cannot be named differently, will be called in a loop
+function draw() {
+  stroke(255);
+  fill(mouseX%255, mouseY%255, 255-(mouseY+mouseX)/100);
+  micLevel = mic.getLevel();
+  ellipse(mouseX, mouseY, 50 + micLevel*500, 50+ micLevel*500);
+}
